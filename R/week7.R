b2c0afd36e1bb8eff2ef03548a423450e196c30e
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-28 15:53:40 -0600

    Plot 2

--- R/week7.R
+++ R/week7.R
@@ -9,7 +9,7 @@ library(GGally)
 week7_tbl <- read_csv("../data/week3.csv") %>%
   mutate(timeStart = ymd_hms(timeStart)) %>%
   mutate(condition = recode_factor(condition, "A" = "Block A", "B" = "Block B", "C" = "Control")) %>%
-  mutate(gender = recode_factor(gender,"F" = "Female", "M" = "Male")) %>%
+  mutate(gender = recode_factor(gender,"M" = "Male", "F" = "Female")) %>%
   filter(q6 == 1) %>%
   select(-q6) %>%
   mutate(timeSpent = timeEnd - timeStart)
@@ -22,7 +22,18 @@ week7_tbl <- read_csv("../data/week3.csv") %>%
 week7_tbl %>%
   select(q1:q10) %>%
   ggpairs()
-week7_tbl %>%
+(week7_tbl %>%
   ggplot(aes(x = timeStart, y = q1)) +
   geom_point() +
-  labs(x = "Date of Experiment", y = "Q1 Score")
+  labs(x = "Date of Experiment", y = "Q1 Score")) %>%
+  ggsave("../figs/fig1.png", ., dpi = 600, height = 3, width = 6)
+
+(week7_tbl %>%
+  ggplot(aes(x = q1, y = q2, color = gender)) +
+  geom_jitter()) %>%
+  ggsave("../figs/fig2.png", ., dpi = 600, height = 4, width = 6.5)
+
+week7_tbl %>%
+  ggplot(aes(x = q1, y = q2)) +
+  geom_jitter() +
+  facet_grid(cols = gender)
